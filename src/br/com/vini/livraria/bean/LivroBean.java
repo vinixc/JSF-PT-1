3362c19211b0ab533b3563ccfbef392559bfd73d
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-21 19:31:37 -0300

    Completando o sistema e lidando com escopos do JSF 2

--- src/br/com/vini/livraria/bean/LivroBean.java
+++ src/br/com/vini/livraria/bean/LivroBean.java
@@ -1,18 +1,44 @@
 package br.com.vini.livraria.bean;
 
-import javax.faces.bean.ManagedBean;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.vini.livraria.dao.DAO;
+import br.com.vini.livraria.entity.Autor;
 import br.com.vini.livraria.entity.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 	
 	private Livro livro = new Livro();
+	private Integer autorId;
 
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
 		
 		this.livro = new Livro();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro(){
+		return this.livro.getAutores();
+	}
+	
+	public List<Autor> getAutores(){
+		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 
 	public Livro getLivro() {
@@ -21,5 +47,13 @@ public class LivroBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
 	}
 }
