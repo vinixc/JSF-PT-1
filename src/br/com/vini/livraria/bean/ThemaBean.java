230c19b50b1dbe372aed0dd4826059be81537617
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-28 14:43:05 -0300

    oferecendo mais temas

--- src/br/com/vini/livraria/bean/ThemaBean.java
+++ src/br/com/vini/livraria/bean/ThemaBean.java
@@ -16,5 +16,16 @@ public class ThemaBean {
 	public void setTema(String tema) {
 		this.tema = tema;
 	}
+	
+	public String[] getTemas() {
+	    return new String[] { "afterdark", "afternoon", "afterwork", "aristo",
+	            "black-tie", "blitzer", "bluesky", "bootstrap", "casablanca",
+	            "cupertino", "cruze", "dark-hive", "delta", "dot-luv",
+	            "eggplant", "excite-bike", "flick", "glass-x", "home",
+	            "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+	            "overcast", "pepper-grinder", "redmond", "rocket", "sam",
+	            "smoothness", "south-street", "start", "sunny", "swanky-purse",
+	            "trontastic", "ui-darkness", "ui-lightness", "vader" };
+	}
 
 }
