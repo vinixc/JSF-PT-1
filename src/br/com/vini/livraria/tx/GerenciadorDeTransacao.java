e4f3fd3766b3335544736bc3a87a46845a63560b
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-31 22:15:07 -0300

    implementando o rollback

--- src/br/com/vini/livraria/tx/GerenciadorDeTransacao.java
+++ src/br/com/vini/livraria/tx/GerenciadorDeTransacao.java
@@ -2,6 +2,8 @@ package br.com.vini.livraria.tx;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
@@ -32,6 +34,14 @@ public class GerenciadorDeTransacao implements Serializable{
 		
 		}catch(Exception e) {
 			manager.getTransaction().rollback();
+			
+			FacesContext context = FacesContext.getCurrentInstance();
+			FacesMessage message = new FacesMessage();
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			message.setSummary(e.getMessage());
+			
+			context.addMessage(null, message);
+			e.printStackTrace();
 			return null;
 		}
 	}
